a3b990812a6c48cd62c523f0aedea208bb51552b
pavanaditya15752 <adi9440116456@gmail.com~>
2023-04-30 12:30:51 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 import Navinshorts from './components/Navinshorts';
 import NewsContent from './components/newscontent/NewsContent';
 import axios from "axios";
-import apikey from './data/config';
 import Footer from './components/Footer/Footer';
+
 
 function App() {
     const [category, setcategory] = useState("general");
@@ -13,10 +13,11 @@ function App() {
     const [newsResults, setnewsResults] = useState();
     const [loadmore, setLoadmore] = useState(20);
 
+
     const newsApi=async()=>{
         try {
-            const proxyUrl="https://cors-anywhere.herokuapp.com/";
-            const news=await axios.get(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}&category=${category}&pageSize=${loadmore}`);
+            
+            const news=await axios.get(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.REACT_APP_API_KEY}&category=${category}&pageSize=${loadmore}`);
 
             setnewsArray(news.data.articles);
             setnewsResults(news.data.totalResults);
--- src/data/config.js
+++
@@ -1,2 +0,0 @@
-const apikey="9c6d0240cafe480ca86192f6d88bee01"
-export default apikey;
